c86819bea0c9f044ca979148307966b631500676
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-09 09:14:40 -0400

    Create customizations.nix

---
+++ customizations.nix
@@ -0,0 +1,24 @@
+{pkgs, ...}: {
+
+  # boot changes
+  boot.kernelPackages = pkgs.linuxPackages_latest; # kernel update
+  
+  # hardware enablement
+  hardware.bluetooth.enable = true; # enable bluetooth
+  
+  # services
+  services.flatpak.enable = true; # flatpak
+  services.hardware.openrgb.enable = true; # openrgb
+
+  # packages
+  environment.systemPackages = with pkgs; [
+    htop
+    wget
+    curl
+    firefox
+    thunderbird
+  ];
+  
+  # programs
+  programs.steam.enable = true; # add steam
+}
