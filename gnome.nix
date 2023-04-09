8ad8716f46a1ee515ebfce7ae6d14ef9a822ebb3
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-09 11:56:03 -0400

    Update gnome.nix

--- gnome.nix
+++ gnome.nix
@@ -3,5 +3,11 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+    
+  # packages
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.dash-to-panel
+    gnome-randr
+  ];
 
 }
