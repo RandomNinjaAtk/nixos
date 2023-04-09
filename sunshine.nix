1067bc3990184f233bdb6d0c99e0903a78819c74
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-09 09:01:16 -0400

    Create sunshine.nix

---
+++ sunshine.nix
@@ -0,0 +1,39 @@
+{pkgs, ...}: {
+
+  environment.systemPackages = with pkgs; [ sunshine ];
+  services.udev.enable = true;
+  services.udev.extraRules = ''
+    # Your rule goes here
+    KERNEL=="uinput", GROUP="input", MODE="0660", OPTIONS+="static_node=uinput"
+  '';
+
+  security.wrappers = {
+    sunshine = {
+      source = "${pkgs.sunshine}/bin/sunshine";
+      capabilities = "cap_sys_admin+ep";
+      owner = "root";
+      group = "root";
+    };
+  }; 
+
+
+  systemd.user.services.sunshine = {
+    script = "/run/current-system/sw/bin/env /run/wrappers/bin/sunshine";
+
+    unitConfig = {
+      Description = "Sunshine is a Game stream host for Moonlight.";
+      StartLimitIntervalSec = 500;
+      StartLimitBurst = 5;
+    };
+
+    serviceConfig = {
+      Environment = "WAYLAND_DISPLAY=wayland-1";
+      # auto restart
+      Restart = "on-failure";
+      RestartSec = "5s";
+    };
+    wantedBy = [ 
+      "graphical-session.target"
+    ];
+  };
+}
