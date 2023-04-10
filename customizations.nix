a25c4732f1bf4d164232f281859dae0807dc38fb
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-10 16:09:10 -0400

    Update customizations.nix

--- customizations.nix
+++ customizations.nix
@@ -39,5 +39,5 @@
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  }; # stean
+  }; # steam
 }
