8b88ba8e62c7d1105e4d4045bcfffd9c07c70f48
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-10 16:39:04 -0400

    Update console.nix

--- console.nix
+++ console.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
 
   # Services
-  services.getty.autologinUser = null; # auto login
+  services.getty.autologinUser = null; # auto login (change null to username)
   
   # packages
   environment.systemPackages = with pkgs; [
