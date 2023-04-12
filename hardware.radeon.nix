f361a595f84e265251f98f31cf0946b5b2e634c6
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-12 15:34:04 -0400

    Update hardware.radeon.nix

--- hardware.radeon.nix
+++ hardware.radeon.nix
@@ -2,42 +2,18 @@
   # boot changes
   boot.initrd.kernelModules = ["amdgpu"]; # enable 
   
+  # hardware
+  hardware.opengl.enable = true; 
+  hardware.opengl.driSupport = true; # enable vulkan
+  hardware.opengl.driSupport32Bit = true; # enable vulkan
+  
   # services
   services.xserver.videoDrivers = ["amdgpu"]; # amd gpu support radv
   
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-    extraPackages = with pkgs; [
-      # OpenCL ICD definition for AMD GPUs using the ROCm stack
-      rocm-opencl-icd
-      # OpenCL runtime for AMD GPUs, part of the ROCm stack
-      rocm-opencl-runtime
-      # AMD Open Source Driver For Vulkan
-      amdvlk
-      # Hardware-accelerated video playpack
-      # VDPAU driver for the VAAPI library
-      vaapiVdpau
-      # VDPAU driver with OpenGL/VAAPI backend
-      libvdpau-va-gl
-      # Tool to query the Video Decode and Presentation API for Unix (VDPAU) abilities of the system
-      vdpauinfo
-    ];
-    extraPackages32 = with pkgs; [
-      driversi686Linux.amdvlk
-    ];
-  };
-   
+ # packages   
  environment.systemPackages = with pkgs; [
-    # Inspect and manipulate PCI devices
-    pciutils
-    # Print all known information about all available OpenCL platforms and devices in the system
-    clinfo
     # Top-like tool for viewing AMD Radeon GPU utilization
     radeontop
-    # Application to read current clocks of AMD Radeon cards
-    radeon-profile
   ];
 }
 
