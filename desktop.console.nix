7d1c55e13bf995441ad253f9eb49707470db7f7b
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-12 22:08:22 -0400

    Update desktop.console.nix

--- desktop.console.nix
+++ desktop.console.nix
@@ -5,7 +5,7 @@
   services.xserver.enable = true;
   
   # Enable the GNOME Desktop Environment  
-   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "gamer";
