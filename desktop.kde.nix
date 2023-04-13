fabd656dc558e07b9a2ff6655c07547314048c55
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-13 15:54:05 -0400

    Update desktop.kde.nix

--- desktop.kde.nix
+++ desktop.kde.nix
@@ -7,6 +7,7 @@
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.defaultSession = "plasmawayland";
 
   # packages
   environment.systemPackages = with pkgs; [
