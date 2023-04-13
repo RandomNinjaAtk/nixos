12a7fc9ffd8e10c5d0691e6c18a13a2c896578ba
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-12 20:21:25 -0400

    Update desktop.kde.nix

--- desktop.kde.nix
+++ desktop.kde.nix
@@ -1,5 +1,9 @@
 {pkgs, ...}: {
 
+  
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
