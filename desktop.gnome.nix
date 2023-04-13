8c370df53b8c1053cc389a0884e168f6124c14a8
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-12 20:21:39 -0400

    Update desktop.gnome.nix

--- desktop.gnome.nix
+++ desktop.gnome.nix
@@ -1,5 +1,8 @@
 {pkgs, ...}: {
 
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
