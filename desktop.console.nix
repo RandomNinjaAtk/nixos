96b393615e98c4b38541cf3a7dab29a9f1104529
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-12 22:14:08 -0400

    Update desktop.console.nix

--- desktop.console.nix
+++ desktop.console.nix
@@ -15,7 +15,7 @@
     # gamescope
   ];
 
-  # autologin workaround
+  # autologin workaround (https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229)
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
 }
