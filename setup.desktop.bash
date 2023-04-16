659e70ae6856b405897fd2d578b295b85abed8f6
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-16 11:52:50 -0400

    Update setup.desktop.bash

--- setup.desktop.bash
+++ setup.desktop.bash
@@ -4,26 +4,27 @@
 # Usage:
 # Run the following command: curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/setup.desktop.bash | bash
 
+cd /etc/nixos
+
 # channel
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/channel.unstable.nix -O /etc/nixos/
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/channel.unstable.nix -O
 
 # desktop
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/desktop.kde.nix -O /etc/nixos/
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/desktop.kde.nix -O
 
 # hardware
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.audio.nix -O /etc/nixos/
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.radeon.nix -O /etc/nixos/
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.bluetooth.nix -O /etc/nixos/
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.razer.nix -O /etc/nixos/
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.audio.nix -O
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.radeon.nix -O
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.bluetooth.nix -O
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.razer.nix -O
 
 # services
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/services.sunshine.nix -O /etc/nixos/
-
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/services.sunshine.nix -O
 # other
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/customizations.nix -O /etc/nixos/
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/customizations.nix -O
 
 # imports
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/imports.desktop.nix -O /etc/nixos/
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/imports.desktop.nix -O
 
 # rebuild
 nixos-rebuild --upgrade-all boot
