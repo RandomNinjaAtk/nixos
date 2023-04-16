d5b41b9b7fc1cb58c2aab1df6242c34759ebfb21
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-16 10:51:03 -0400

    Create setup.desktop.bash

---
+++ setup.desktop.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+# channel
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/channel.unstable.nix -O /etc/nixos/
+
+# desktop
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/desktop.kde.nix -O /etc/nixos/
+
+# hardware
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.audio.nix -O /etc/nixos/
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.radeon.nix -O /etc/nixos/
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.bluetooth.nix -O /etc/nixos/
+
+# services
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/services.sunshine.nix -O /etc/nixos/
+
+#
+nixos-rebuild --upgrade-all boot
