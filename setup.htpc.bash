278e74791eca34108ee17b84da4b5fa4c5f92819
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-16 11:55:41 -0400

    Update setup.htpc.bash

--- setup.htpc.bash
+++ setup.htpc.bash
@@ -4,23 +4,25 @@
 # Usage:
 # Run the following command: curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/setup.htpc.bash | bash
 
+cd /etc/nixos
+
 # channel
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/channel.unstable.nix -O /etc/nixos/
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/channel.unstable.nix -O
 
 # desktop
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/desktop.kde.nix -O /etc/nixos/
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/desktop.kde.nix -O
 
 # hardware
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.audio.nix -O /etc/nixos/
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.radeon.nix -O /etc/nixos/
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.bluetooth.nix -O /etc/nixos/
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.audio.nix -O
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.radeon.nix -O
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.bluetooth.nix -O
 
 # services
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/services.sunshine.nix -O /etc/nixos/
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/services.steam.nix -O /etc/nixos/
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/services.sunshine.nix -O
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/services.steam.nix -O
 
 # imports
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/imports.htpc.nix -O /etc/nixos/
+curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/imports.htpc.nix -O
 
 # rebuild
 nixos-rebuild --upgrade-all boot
