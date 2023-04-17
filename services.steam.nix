604505fddf761ac5ce06c988c92eb54bc81bbbf4
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-17 14:36:57 -0400

    Update services.steam.nix

--- services.steam.nix
+++ services.steam.nix
@@ -3,8 +3,8 @@
  # programs
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    #remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    #dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   }; # steam
   
   # steam hardware
