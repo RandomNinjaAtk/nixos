cea2b833712c673918b0aca6a5d23afe24d817b5
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-17 14:40:08 -0400

    Create packages.htpc.nix

---
+++ packages.htpc.nix
@@ -0,0 +1,21 @@
+{pkgs, ...}: {
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  
+  # Remove packages
+  services.xserver.excludePackages = [ pkgs.xterm ]; # remove xterm
+
+  # packages
+  environment.systemPackages = with pkgs; [
+    mlocate
+    wget
+    curl
+    firefox
+    nextcloud-client
+    gping
+    wayvnc
+    appimage-run
+    vlc
+  ];
+}
