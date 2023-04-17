8562fff27edf9296872661cafc99c3e3805a66c6
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-17 14:17:48 -0400

    Update customizations.nix

--- customizations.nix
+++ customizations.nix
@@ -25,28 +25,7 @@
     publish.workstation = true;
   };
 
-  
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
 
-  # packages
-  environment.systemPackages = with pkgs; [
-    mlocate
-    vscode
-    htop
-    wget
-    curl
-    firefox
-    thunderbird
-    discord
-    signal-desktop
-    nextcloud-client
-    gping
-    wayvnc
-    rpi-imager
-    appimage-run
-    vmware-horizon-client
-  ];
   
   # programs
   programs.steam = {
