6fa965fd8c9afb55f34f85e52551e93e83d21255
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-17 14:31:23 -0400

    Update packages.desktop.nix

--- packages.desktop.nix
+++ packages.desktop.nix
@@ -9,8 +9,8 @@
   # programs
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    #remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    #dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   }; # steam
 
   # packages
