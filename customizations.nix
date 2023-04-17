418b2805e9144626636d54ae04bea17741394dcb
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-17 14:24:33 -0400

    Update customizations.nix

--- customizations.nix
+++ customizations.nix
@@ -1,8 +1,5 @@
 {pkgs, ...}: {
-
-  # boot changes
-  boot.kernelPackages = pkgs.linuxPackages_latest; # kernel update
-    
+  
   # hardware enablement
   hardware.uinput.enable = true; # enable uinput
   hardware.steam-hardware.enable = true; # steam hardware
@@ -12,15 +9,6 @@
   services.fwupd.enable = true; # firmware updates
   
 
-
-  
-  # programs
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  }; # steam
-  
   # other
   xdg.portal.wlr.enable = true; # wayland compositor for screensharing
 }
