275378932aa6152de4e3adf0ebfd395de43cbab3
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-17 14:29:45 -0400

    Update customizations.nix

--- customizations.nix
+++ customizations.nix
@@ -1,14 +1,7 @@
 {pkgs, ...}: {
-  
-  # hardware enablement
-  hardware.uinput.enable = true; # enable uinput
-  hardware.steam-hardware.enable = true; # steam hardware
-      
+        
   services.fprintd.enable = true; # finger print reader
   services.flatpak.enable = true; # flatpak
   services.fwupd.enable = true; # firmware updates
   
-
-  # other
-  xdg.portal.wlr.enable = true; # wayland compositor for screensharing
 }
