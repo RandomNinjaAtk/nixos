ab58c55c6c0f089cb40c4cf83de28311b65f33e4
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-18 13:50:25 -0400

    Update desktop.htpc.nix

--- desktop.htpc.nix
+++ desktop.htpc.nix
@@ -7,7 +7,7 @@
   # KDE
   services.xserver.desktopManager.plasma5.enable = true;
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.defaultSession = "plasmawayland";
+  services.xserver.displayManager.defaultSession = "plasma-big-screen-wayland"; #plasmawayland or plasma-big-screen-wayland
   services.xserver.desktopManager.plasma5.bigscreen.enable = true;
   
   # Auto Login
