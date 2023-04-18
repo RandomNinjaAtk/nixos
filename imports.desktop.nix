72c6454066f647252525d6a71cca85f82f4789c5
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-18 08:26:53 -0400

    Update imports.desktop.nix

--- imports.desktop.nix
+++ imports.desktop.nix
@@ -1,16 +1,16 @@
 { config, pkgs, ... }: {
   imports =
     [ 
+      ./desktop.kde.nix
+      ./packages.desktop.nix
       ./kernel.nix
       ./channel.unstable.nix
-      ./desktop.kde.nix
       ./hardware.audio.nix
       ./hardware.radeon.nix
       ./hardware.bluetooth.nix
       ./hardware.razer.nix
       ./services.sunshine.nix
       ./services.openrgb.nix
-      ./packages.desktop.nix
       ./networking.nix
       ./customizations.nix
     ];
