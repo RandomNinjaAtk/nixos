d3d5753bc86edad5bf9acf82dacee11521c9582f
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-26 13:10:13 -0400

    Update enable.printing.nix

--- enable.printing.nix
+++ enable.printing.nix
@@ -4,6 +4,12 @@
   services.avahi.nssmdns = true;
   # for a WiFi printer
   services.avahi.openFirewall = true;
+  
+  # additional drivers
+  services.printing.drivers = [ 
+    pkgs.gutenprint
+    pkgs.gutenprintBin 
+  ];
 }
 
 # Reference: https://nixos.wiki/wiki/Printing
