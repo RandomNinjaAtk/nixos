218feb318c886cb4c664f61441a6e9fa23aed109
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-04-27 16:25:40 -0400

    Update setup.desktop.bash

--- setup.desktop.bash
+++ setup.desktop.bash
@@ -8,48 +8,65 @@ cd /etc/nixos
 
 # kernel
 echo "Getting Kernel Configurations"
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/kernel.nix -O
+curl -s https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/kernel.nix -O
+echo "Done"
 
 # channel
 echo "Getting Nix Channel Configurations"
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/channel.unstable.nix -O
+curl -s https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/channel.unstable.nix -O
+echo "Done"
 
 # desktop
 echo "Getting desktop.kde.nix"
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/desktop.kde.nix -O
+curl -s https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/desktop.kde.nix -O
+echo "Done"
 
 # hardware
 echo "Getting Hardware configurations"
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.audio.nix -O
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.radeon.nix -O
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.bluetooth.nix -O
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.razer.nix -O
+curl -s https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.audio.nix -O
+curl -s https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.radeon.nix -O
+curl -s https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.bluetooth.nix -O
+curl -s https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/hardware.razer.nix -O
+echo "Done"
 
 # networking
 echo "Getting Network configurations"
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/networking.nix -O
+curl -s https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/networking.nix -O\
+echo "Done"
 
 # printing
 echo "Getting Printer configurations"
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/enable.printing.nix -O
+curl -s https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/enable.printing.nix -O
+echo "Done"
 
 # services
 echo "Getting Services configurations"
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/services.sunshine.nix -O
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/services.openrgb.nix -O
+curl -s https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/services.sunshine.nix -O
+curl -s https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/services.openrgb.nix -O
+echo "Done"
 
 # packages
 echo "Getting Desktop Packages configurations"
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/packages.desktop.nix -O
+curl -s https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/packages.desktop.nix -O
+echo "Done"
 
 # other
 echo "Getting Other configurations"
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/customizations.nix -O
+curl -s https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/customizations.nix -O
+echo "Done"
 
 echo "Getting Nix import configuration"
 # imports
-curl https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/imports.desktop.nix -O
+curl -s https://raw.githubusercontent.com/RandomNinjaAtk/nixos/main/imports.desktop.nix -O
+echo "Done"
 
 # rebuild
+echo "Performing NixOS garbage collection, older than 5 days..."
+nix-collect-garbage --delete-older-than 5d
+echo "Done"
+echo "Performing NixOS Rebuild, Upgrade and Boot..."
 nixos-rebuild --upgrade-all boot
+echo "Done"
+echo "Reboot system for changes to take affect..."
+echo "Exiting...."
 exit
