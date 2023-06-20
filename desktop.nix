a5d5688def8e77f5c35db91a3f295e05ae1c5156
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-06-20 15:52:11 -0400

    Create desktop.nix

---
+++ desktop.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./services.sunshine.nix
+    ];
+    
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  
+  # Packages
+  environment.systemPackages = with pkgs; [
+    curl
+    wget
+    htop
+    signal-desktop
+    discord
+    rpi-imager
+    ungoogled-chromium
+    firefox
+    thunderbird
+    openrgb-with-all-plugins
+    razergenie
+    moonlight-qt
+  ];
+
+  hardware.bluetooth.enable = true;
+  hardware.openrazer.enable = true;
+  services.openssh.enable = true;
+  services.hardware.openrgb.enable = true;
+  services.sunshine.enable = true;
+  services.tailscale.enable = true;
+  networking.firewall.enable = false;
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+}
