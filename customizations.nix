9612362be708114e91171488274f10b29d18519d
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-06-20 15:45:22 -0400

    Update customizations.nix

--- customizations.nix
+++ customizations.nix
@@ -3,5 +3,6 @@
   services.fprintd.enable = true; # finger print reader
   services.flatpak.enable = true; # flatpak
   services.fwupd.enable = true; # firmware updates
+  services.sunshine.enable = true; sunshine
   
 }
