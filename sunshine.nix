09ce484089f32d94ed5fa21a7ea5807c6ba4b2ea
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-06-22 10:53:22 -0400

    Update sunshine.nix

--- sunshine.nix
+++ sunshine.nix
@@ -1,18 +1,44 @@
-{pkgs, ...}: {
+{ config, lib, pkgs, ... }:
 
-  # Install sunshine
-  environment.systemPackages = with pkgs; [ sunshine ];
+with lib;
+
+let
+
+  cfg = config.services.sunshine;
+
+in
+
+{
+  options = {
+
+    services.sunshine = {
+      enable = mkEnableOption (mdDoc "Sunshine");
+    };
+
+  };
+
+  config = mkIf config.services.sunshine.enable {
+
+    # Install sunshine
+    environment.systemPackages = with pkgs; [ sunshine ];
   
-  systemd.user.services.sunshine = {
-    description = "Sunshine self-hosted game stream host for Moonlight.";
-    startLimitIntervalSec = 500;
-    startLimitBurst = 5;
-    wantedBy = [ "graphical-session.target" ];
-
-    serviceConfig = {
-      Restart = "on-failure";
-      RestartSec = "5s";
-      ExecStart = "${pkgs.sunshine}/bin/sunshine";
+    # Requires to simulate input
+    boot.kernelModules = [ "uinput" ];
+    services.udev.extraRules = ''
+      KERNEL=="uinput", SUBSYSTEM=="misc", OPTIONS+="static_node=uinput", TAG+="uaccess"
+    '';
+    
+    systemd.user.services.sunshine = {
+      description = "Sunshine self-hosted game stream host for Moonlight.";
+      startLimitIntervalSec = 500;
+      startLimitBurst = 5;
+      wantedBy = [ "graphical-session.target" ];
+  
+      serviceConfig = {
+        Restart = "on-failure";
+        RestartSec = "5s";
+        ExecStart = "${pkgs.sunshine}/bin/sunshine";
+      };
     };
   };
 }
