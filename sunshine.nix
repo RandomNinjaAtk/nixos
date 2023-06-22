0255a76e382c1d47a5a8eb12eddddb2c69576e0c
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-06-22 11:13:32 -0400

    Update sunshine.nix

--- sunshine.nix
+++ sunshine.nix
@@ -29,6 +29,8 @@ in
     '';
 
     security.wrappers.sunshine = {
+      owner = "root";
+      group = "root";
       capabilities = "cap_sys_admin+p";
       source = "${pkgs.sunshine}/bin/sunshine";
     };
