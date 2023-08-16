bcfc98a695fbb73919d5637a67c8ef3502784464
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-08-16 07:47:43 -0400

    Update desktop.nix

--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -99,6 +99,22 @@
     distrobox
   ];
 
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+      # For Nixos version > 22.11
+      #defaultNetwork.settings = {
+      #  dns_enabled = true;
+      #};
+    };
+  };
+
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
