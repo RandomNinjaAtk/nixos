f9a5bbb1de39141633d0bf043cd8874d34caf1d2
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-08-17 17:17:19 +0000

    Update install.flatpak.apps.bash

--- hosts/install.flatpak.apps.bash
+++ hosts/install.flatpak.apps.bash
@@ -6,7 +6,7 @@ flatpak -y install flathub \
   com.valvesoftware.Steam \
   com.valvesoftware.SteamLink \
   com.valvesoftware.Steam.CompatibilityTool.Boxtron \
-  com.github.Matoking.protontricks \ #org.freedesktop.Platform.VulkanLayer.MangoHud 
+  com.github.Matoking.protontricks \
   org.freedesktop.Platform.VulkanLayer.vkBasalt \
   com.valvesoftware.Steam.Utility.gamescope \
   com.discordapp.Discord \
