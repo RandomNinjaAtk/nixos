cbdd298688ed8cef97ef35724798afbed6f3586e
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-08-18 11:12:24 +0000

    Update services.steam.nix

--- services.steam.nix
+++ services.steam.nix
@@ -5,8 +5,8 @@
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    gamescopeSession.enable = false; # Whether to enable GameScope Session.
-    gamescopeSession = true; # Run a GameScope driven Steam session from your display-manager
+    gamescopeSession.enable = true; # Whether to enable GameScope Session.
+    #gamescopeSession = true; # Run a GameScope driven Steam session from your display-manager
   }; # steam
   
   # steam hardware
