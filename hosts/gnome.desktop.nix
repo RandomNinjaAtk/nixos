1dd105a067627f00afcb6ba8fa84eaad0b9a6f2b
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-08-19 15:08:54 -0400

    Update gnome.desktop.nix

--- hosts/gnome.desktop.nix
+++ hosts/gnome.desktop.nix
@@ -65,6 +65,7 @@
   services.fprintd.enable = true; # finger print reader
   services.flatpak.enable = true; # flatpak
   services.fwupd.enable = true; # firmware updates
+  services.locate.enable = true; # enable locate services
 
   # Print Services
   services.printing.enable = true; # enable printing
@@ -122,6 +123,7 @@
     openrgb-with-all-plugins
     gamescope
     mangohud
+    wget
     # APPLICATIONS
     picard
     moonlight-qt
