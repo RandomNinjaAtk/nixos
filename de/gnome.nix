087977b0392f1f37dae038a1d74e56fde8b97348
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2024-01-29 11:27:28 -0500

    Create gnome.nix

---
+++ de/gnome.nix
@@ -0,0 +1,79 @@
+{ config, pkgs, ... }:
+let
+  unstableTarball =
+    fetchTarball
+      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;  
+in
+{
+  nixpkgs.config = {
+    packageOverrides = pkgs: {
+      unstable = import unstableTarball {
+        config = config.nixpkgs.config;
+      };
+    };
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # gnome
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+  services.gnome.gnome-remote-desktop.enable = true;
+  services.xserver.displayManager.gdm.wayland = true; # Allow GDM to run on Wayland instead of Xserver
+  services.xserver.desktopManager.gnome.extraGSettingsOverrides = ''
+    [org.gnome.desktop.interface]
+    gtk-theme='Dracula'
+
+    [org.gnome.desktop.wm.preferences]
+    button-layout=':minimize,maximize,close'
+    resize-with-right-button=true
+    theme='Dracula'
+
+    [org.gnome.desktop.interface.icon-theme]
+    theme='Dracula'
+
+    [org.gnome.SessionManager]
+    auto-save-session=true
+
+    [org.gtk.Settings.FileChooser]
+    sort-directories-first=true
+  '';
+
+  # https://github.com/rhoriguchi/nixos-setup/blob/master/modules/profiles/gnome.nix
+  environment.gnome.excludePackages = [
+      pkgs.gnome-tour
+      pkgs.gnome.geary
+    ];
+
+  # Remove packages
+  services.xserver.excludePackages = [ pkgs.xterm ]; # remove xterm
+
+  # Packages
+  environment.systemPackages = with pkgs; [
+    # EXTENSIONS
+    gnome.gnome-tweaks
+    gnomeExtensions.appindicator
+    gnomeExtensions.alphabetical-app-grid
+    gnomeExtensions.dash-to-panel
+    gnomeExtensions.arcmenu
+    gnomeExtensions.wireless-hid
+    gnomeExtensions.simple-system-monitor
+    gnomeExtensions.tiling-assistant
+    gnomeExtensions.no-overview
+    # GNOME APPS
+    gnome.gnome-tweaks
+    gnome-firmware
+    gnome-console
+    gnome.gnome-boxes
+    gnome-photos
+    gnome-usage
+    # THEMES
+    dracula-theme
+    dracula-icon-theme
+    nordic
+    gnome.adwaita-icon-theme
+    beauty-line-icon-theme
+  ];
+}
