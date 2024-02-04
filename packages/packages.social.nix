af9267a70bb5b751a86c39b1a9e0494eb92c5af1
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2024-02-04 17:08:07 -0500

    Create packages.social.nix

---
+++ packages/packages.social.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+let
+  unstableTarball =
+    fetchTarball
+      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;  
+in
+{
+  nixpkgs.config = {
+    packageOverrides = pkgs: {
+      unstable = import unstableTarball {
+        config = config.nixpkgs.config;
+      };
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    discord
+    signal-desktop
+    zoom-us
+  ];
+}
