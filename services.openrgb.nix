d6425f1bd9e0de35bf1d48be634f86f4b62372aa
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2024-03-10 14:24:51 -0400

    Update services.openrgb.nix

--- services.openrgb.nix
+++ services.openrgb.nix
@@ -1,4 +1,17 @@
-{pkgs, ...}: {
+{ config, pkgs, ... }:
+let
+  unstableTarball =
+    fetchTarball
+      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;  
+in
+{
+  nixpkgs.config = {
+    packageOverrides = pkgs: {
+      unstable = import unstableTarball {
+        config = config.nixpkgs.config;
+      };
+    };
+  };
   # Enable openrgb
   services.hardware.openrgb = {
     enable = true;
