8861e7dc2418ead492c3f8e91468c37e5f4e11da
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2024-03-17 12:22:56 -0400

    Update services.openrgb.nix

--- services.openrgb.nix
+++ services.openrgb.nix
@@ -1,31 +1,29 @@
-{ config, pkgs, ... }:
+{ config, lib,  pkgs, ... }:
+with lib;
 let
   cfg = config.services.openrgb;
-  unstableTarball =
-    fetchTarball
-      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;  
 in
 {
-  nixpkgs.config = {
-    packageOverrides = pkgs: {
-      unstable = import unstableTarball {
-        config = config.nixpkgs.config;
-      };
+    
+  options = {
+    services.openrgb = {
+      enable = mkEnableOption (mdDoc "OpenRGB");
     };
   };
-  # Enable openrgb
-  services.hardware.openrgb = {
-    enable = true;
-    package = pkgs.openrgb-with-all-plugins;
-  };
-  systemd.user.services.openrgb =
-    {
+  config = mkIf config.services.openrgb.enable {
+    # Enable openrgb
+    services.hardware.openrgb = {
+      enable = true;
+      package = pkgs.openrgb-with-all-plugins;
+    };
+    systemd.user.services.openrgb = {
       description = "openrgbstart";
       wantedBy = [ "graphical-session.target" ];
       serviceConfig = {
         ExecStart = "${pkgs.openrgb-with-all-plugins}/bin/openrgb --startminimized";
       };
     };
+  };
 }
 
 # Enable using:
