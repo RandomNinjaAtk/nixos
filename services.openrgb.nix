b97dae887bb2411c2cbf28eb6bd8ca0e16a33933
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2024-03-17 12:11:17 -0400

    Update services.openrgb.nix

--- services.openrgb.nix
+++ services.openrgb.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }:
 let
+  cfg = config.services.openrgbstart;
   unstableTarball =
     fetchTarball
       https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;  
@@ -17,4 +18,19 @@ in
     enable = true;
     package = pkgs.openrgb-with-all-plugins;
   };
+  systemd.user.services.openrgbstart =
+    {
+      description = "openrgbstart";
+      wantedBy = [ "graphical-session.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.openrgb-with-all-plugins}/bin/openrgb --startminimized";
+      };
+    };
 }
+
+# Enable using:
+# services.openrgbstart.enable = true;
+# Get Service Status
+# systemctl --user status openrgbstart
+# get logs
+# journalctl --user -u openrgbstart --since "2 minutes ago"
