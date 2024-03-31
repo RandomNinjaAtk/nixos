94dbbf97adc5b697ff830d89a9d4b692b73c24fb
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2024-03-31 17:00:12 -0400

    Update desktop_new.nix

--- hosts/desktop_new.nix
+++ hosts/desktop_new.nix
@@ -168,6 +168,7 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    git
     wget
     curl
     clinfo
