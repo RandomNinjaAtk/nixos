e21032e42104896c1536ea5e4aded047161b8fea
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2025-01-29 14:06:49 -0500

    Declarative flatpaks!

---
+++ services/flatpak.nix
@@ -0,0 +1,69 @@
+{ config, pkgs, ... }:
+
+let
+  # We point directly to 'gnugrep' instead of 'grep'
+  grep = pkgs.gnugrep;
+
+  # 1. Declare the Flatpaks you *want* on your system
+  desiredFlatpaks = [
+    # Utilities
+    "com.kgurgul.cpuinfo"
+    "org.gnome.Firmware"
+    "com.rustdesk.RustDesk"
+    "com.bitwarden.desktop"
+    # Productivity
+    "org.mozilla.firefox"
+    "org.mozilla.Thunderbird"
+    "org.libreoffice.LibreOffice"
+    "com.vscodium.codium"
+    "com.brave.Browser"
+    "info.portfolio_performance.PortfolioPerformance"
+    "io.gitlab.news_flash.NewsFlash"
+    # Media
+    "com.github.KRTirtho.Spotube"
+    "com.stremio.Stremio"
+    "tv.plex.PlexHTPC"
+    "tv.plex.PlexDesktop"
+    "tv.kodi.Kodi"
+    # Social
+    "org.telegram.desktop"
+    "com.discordapp.Discord"
+    # Gaming
+    "page.kramo.Cartridges"
+    "io.github.hmlendea.geforcenow-electron"
+    "com.heroicgameslauncher.hgl"
+    "com.moonlight_stream.Moonlight"
+    "org.libretro.RetroArch"
+    "com.valvesoftware.SteamLink"
+    "net.davidotek.pupgui2" # ProtonUp-QT
+  ];
+in
+{
+  system.activationScripts.flatpakManagement = {
+    text = ''
+      # 2. Ensure the Flathub repo is added
+      ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub \
+        https://flathub.org/repo/flathub.flatpakrepo
+
+      # 3. Get currently installed Flatpaks
+      installedFlatpaks=$(${pkgs.flatpak}/bin/flatpak list --app --columns=application)
+
+      # 4. Remove any Flatpaks that are NOT in the desired list
+      for installed in $installedFlatpaks; do
+        if ! echo ${toString desiredFlatpaks} | ${grep}/bin/grep -q $installed; then
+          echo "Removing $installed because it's not in the desiredFlatpaks list."
+          ${pkgs.flatpak}/bin/flatpak uninstall -y --noninteractive $installed
+        fi
+      done
+
+      # 5. Install or re-install the Flatpaks you DO want
+      for app in ${toString desiredFlatpaks}; do
+        echo "Ensuring $app is installed."
+        ${pkgs.flatpak}/bin/flatpak install -y flathub $app
+      done
+
+      # 6. Update all installed Flatpaks
+      ${pkgs.flatpak}/bin/flatpak update -y
+    '';
+  };
+}
