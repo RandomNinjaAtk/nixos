be61093f215dcb0e0b7d8ca0aab0f76a9241f6be
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2025-04-28 09:08:43 -0400

    Create gnome.nix

---
+++ hosts/workstation-01/gnome.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+let
+  unstableTarball =
+    fetchTarball
+      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
+in
+{
+  nixpkgs.config = {
+    packageOverrides = pkgs: {
+      unstable = import unstableTarball {
+        config = config.nixpkgs.config;
+      };
+    };
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+}
