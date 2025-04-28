991eccdaffab3a9917419140daf6f995fa0c2fdc
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2025-04-28 09:10:06 -0400

    Create steam.nix

---
+++ hosts/workstation-01/steam.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, ... }:
+let
+  unstableTarball =
+    fetchTarball
+      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
+in
+{
+  nixpkgs.config = {
+    packageOverrides = pkgs: {
+      unstable = import unstableTarball {
+        config = config.nixpkgs.config;
+      };
+    };
+  };
+  
+  # steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers.
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    protontricks.enable = true; # Whether to enable protontricks, a simple wrapper for running Winetricks commands for Proton-enabled games.
+  };
+ 
+  # https://github.com/fabiscafe/game-devices-udev
+  services = {
+    udev = {
+      packages = with pkgs; [
+        game-devices-udev-rules
+      ];
+    };
+  };
+  hardware.uinput.enable = true;
+
+  #systemd.user.services.steam = {
+  #  enable = true;
+  #  description = "Open Steam in the background at boot";
+  #  wants = [ "network-online.target" ];
+  #  after = [ "network-online.target" ];
+    #wants = [ "graphical-session.target" ];
+    #partOf = [ "graphical-session.target" ];
+    #after = [ "graphical-session.target" ];
+  #  startLimitBurst = 5;
+  #  wantedBy = ["default.target"];
+  #  startLimitIntervalSec = 500;
+  #  serviceConfig = {
+  #    ExecStart = "${pkgs.steam}/bin/steam -nochatui -nofriendsui -silent %U";
+  #    Restart = "on-failure";
+  #    RestartSec = "5s";
+  #  };
+  #};
+}
