98aec2964c19d74bb0fa75b2ff45b298d1a54288
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2025-04-28 09:04:35 -0400

    Create amd.nix

---
+++ hosts/workstation-01/amd.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+let
+  unstableTarball =
+    fetchTarball
+      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
+in
+{
+  nixpkgs.config = {
+    packageOverrides = pkgs: {
+      unstable = import unstableTarball {
+        config = config.nixpkgs.config;
+      };
+    };
+  };
+
+  #boot.initrd.kernelModules = [ "amdgpu" ];
+  #services.xserver.videoDrivers = [ "amdgpu" ];
+
+
+  # Information: https://wiki.nixos.org/wiki/AMD_GPU
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [ rocmPackages.clr.icd ];
+  };
+
+  environment.systemPackages = with pkgs; [ lact ];
+  systemd.packages = with pkgs; [ lact ];
+  systemd.services.lactd.wantedBy = ["multi-user.target"];
+}
