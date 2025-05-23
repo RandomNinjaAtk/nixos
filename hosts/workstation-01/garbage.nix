8ffda2956eaaaabfd2ee22b71a89fb9c0a6de86f
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2025-05-23 11:31:35 -0400

    Create garbage.nix

---
+++ hosts/workstation-01/garbage.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }:
+let
+  unstableTarball =
+    fetchTarball
+      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
+in
+{
+  nixpkgs.config = {
+    packageOverrides = pkgs: {
+      unstable = import unstableTarball {
+        config = config.nixpkgs.config;
+      };
+    };
+  };
+ 
+  # https://wiki.nixos.org/wiki/Storage_optimization
+  nix.settings.auto-optimise-store = true;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+  
+  nix.extraOptions = ''
+    min-free = ${toString (100 * 1024 * 1024)}
+    max-free = ${toString (1024 * 1024 * 1024)}
+  '';
+
+}
